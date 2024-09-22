fee052521a2573a2acc23626f683bef18e4fe6b3
Shivam-developer-01 <shivamicrosoftsign001@gmail.com>
2024-09-22 13:24:02 +0000

    Basic Datatype conversion

---
+++ Basics_JS_01/Conversion.js
@@ -0,0 +1,48 @@
+
+// conversion in of different datatypes 
+//let target = 75
+//let target = 40
+// let target = "33xyz"
+let target = null
+//let target = undefined 
+let value = 80
+//console.log( typeof target)                    
+//console.log( target) 
+let valueInumber = Number(target)  
+//console.log(typeof valueInumber) 
+//console.log(valueInumber)  
+/* 33 => 33
+    "33xyz" => NaN  not a number 
+    true or false => either 1 or 0
+*/
+ let checkBoolean = 123
+ let checkString = "HelloWorld"
+ let checkString2 = ""
+ let BOOLEAN = Boolean(checkString) 
+ let BOOLEAN2 = Boolean(checkString2) 
+ //console.log( BOOLEAN) 
+ //console.log(BOOLEAN2) 
+ /*
+    1 or 125 => true
+    "helloworld" => true
+    ""  => false 
+ */
+let getforString = 55
+let ChangeString = String(getforString)   
+// console.log(ChangeString) 
+
+          ////////     Operations ////////  
+         
+          // string Operations 
+     let str1 = "Hello"
+     let str2 = "Papa"
+     // console.log(str1+str2) 
+
+     console.log( "1"+ 2)
+     console.log( 1+ "2")
+     console.log( "1"+ 2 + 2)
+     console.log( 2 + 2 + "2")  
+
+
+
+
