e6b20bec8a007aa4af4c5c84db82c7de6d41d367
Shivam-developer-01 <shivamicrosoftsign001@gmail.com>
2024-09-22 07:45:07 +0000

    Fundamentals

---
+++ Basics_JS_01/Datatypes02.js
@@ -0,0 +1,22 @@
+
+let name = " username"
+let age = 10
+let IsMarried = false 
+
+/* Datatypes in Javascript language 
+  number   somewhere 2 to power 53 
+  bigint   used for bigger dataypes integer 
+  string
+  boolean   Either True/ False
+  null      standalone value
+  undefined   variable is declared but not defined what value in it and its datatype,,  value not assigned in varible 
+  symbol      used for uniqueness in component 
+  object 
+
+*/
+// console.table([typeof name, age, IsMarried]) 
+console.log(typeof age)
+console.log(typeof IsMarried) 
+
+console.log(typeof null)    // it is imp to know that null type is object and undefined type is undefined itself, imp interview point
+console.log(typeof undefined) 
---
+++ Basics_JS_01/Variables_JS.js
@@ -0,0 +1,15 @@
+
+const UserAccount = 90125;
+let EmailID = "user123@gmailcom";
+var PhoneNumber = "1245793325";
+ UserCity = "Noida";            // variables can be declared in JS without using Const, let, var. OK 
+  
+ let RelationshipStatus = " Single";
+ let UserFamily;                     // variable declared but not defined thats why in terminal, value is undefined. Ok 
+  PhoneNumber = "4893132143"; 
+  UserCity = "Delhi";                 // Variables have been updated in terminal result of code. OK 
+
+  console.table([ UserAccount, EmailID, UserCity, PhoneNumber,RelationshipStatus,UserFamily]);   // console.table([]) used for printing
+                                                                                                  // multiple values in a single one. OK
+  
+
